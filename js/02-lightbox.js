ce05ce5538e350b850359781fd19e2abc9cf0233
Roman Dachko <roman8301@gmail.com>
2021-11-07 15:54:48 +0200

    Task 2 (02-lightbox.js) completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryWrap = document.querySelector('.gallery');
+renderGallary();
+
+function renderGallary() {
+  const gallaryItem = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+  galleryWrap.insertAdjacentHTML('beforeend', gallaryItem);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
